4577081d7f784cdd14cfcb87f4fbe4f82f9ba3ef
Aman Sharma <mannu.poski10@gmail.com>
2018-06-11 19:59:35 +0530

    In accordance with PEP8 guidelines

--- stream/users/serializers.py
+++ stream/users/serializers.py
@@ -1,19 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
+
 class UserSerializer(serializers.ModelSerializer):
+
     password = serializers.CharField(write_only=True)
-    confirm_password = serializers.CharField(write_only=True, required=True) 
-    first_name = serializers.CharField(required = True)
-    last_name = serializers.CharField(required = True)
- 
+    confirm_password = serializers.CharField(write_only=True, required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+
     def create(self, validated_data):
         if validated_data['password'] == validated_data['confirm_password']:
             user = get_user_model().objects.create(
-                first_name = validated_data['first_name'],
-                last_name = validated_data['last_name'],
-                username = validated_data['username'],
-                is_active = False,
+                first_name=validated_data['first_name'],
+                last_name=validated_data['last_name'],
+                username=validated_data['username'],
+                is_active=False,
             )
             user.set_password(validated_data['password'])
             user.save()
@@ -22,5 +24,6 @@ class UserSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Passwords don't match")
 
     class Meta:
+
         model = get_user_model()
         fields = ('first_name', 'last_name', 'username', 'password', 'confirm_password')
--- stream/users/views.py
+++ stream/users/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from users.serializers import UserSerializer
 from rest_framework import generics
 from django.contrib.auth import get_user_model
@@ -6,16 +5,19 @@ from rest_framework.response import Response
 
 # Create your views here.
 
-class CreateUserView(generics.CreateAPIView): #Provides only POST Method
+
+class CreateUserView(generics.CreateAPIView):  # Provides only POST Method
+
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
 
+
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-    
+
     def get(self, request, username):
-        user = get_user_model().objects.get(username=username) 
+        user = get_user_model().objects.get(username=username)
         serializer = UserSerializer(user)
         return Response(serializer.data)
-        
