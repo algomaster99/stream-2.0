0a923663b73a6081ea58f4befd26573fde03d07e
Aman Sharma <mannu.poski10@gmail.com>
2018-07-06 00:34:29 +0530

    added styles to controller

--- stream-frontend/src/streamController.js
+++ stream-frontend/src/streamController.js
@@ -73,7 +73,17 @@ export default class StreamController extends React.Component {
         mute: data['mute'],
         duration: data['duration'],
         seek: data['seek'],
-      });
+      }, () => {
+          if (!this.state.duration) {
+      document.querySelector('.slider').style.background = "linear-gradient(90deg, #ffd626 0%, #919191 0%)"
+      console.log('inital');
+    }
+    else {
+      let percentage = (this.state.seek/this.state.duration)*100
+      document.querySelector('.slider').style.background = "linear-gradient(90deg, #ffd626 "+percentage+"%, #919191 0%)"
+    console.log('change');
+    }
+         });
     }
     console.log("componentMount streamCon.js");
   }
@@ -86,8 +96,10 @@ export default class StreamController extends React.Component {
         <Icon
          name={this.state.mute ? "volume off" : "volume up"}
          onClick={this.handleMute}>
-        </Icon><br />
-        <input type="range" name="seek" min="0" max={this.state.duration} value={this.state.seek} onChange={this.handleSeek} />
+        </Icon>
+        <div className="slider-container">
+        <input type="range" name="seek" min="0" max={this.state.duration} value={this.state.seek} onChange={this.handleSeek} className='slider' />
+        </div>
         <p>{moment("0").seconds(this.state.seek).format('mm:ss')+"/"+moment("0").seconds(this.state.duration).format('mm:ss')}</p>
       </Segment>
       </div>
